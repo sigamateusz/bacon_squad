cdfa00cc24f2bef9faf9d04d47bcaa2aecd1ca9f
sebmichalowski <seb.michalowski@gmail.com>
2017-02-25 17:38:39 +0100

    login_model

--- login_model.py
+++ login_model.py
@@ -14,5 +14,5 @@ class LoginModel:
         :return: list
         """
         sql_obj = sql.query("SELECT ID, MAIL FROM USERS WHERE MAIL=? AND PASSWORD=?;", [mail, password])
-        mail_pasword_list = [sql_obj[0][0], sql_obj[0][1]]
-        return mail_pasword_list
+        mail_password_list = [sql_obj[0][0], sql_obj[0][1]]
+        return mail_password_list
