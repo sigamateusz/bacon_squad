f7a9c40519e68a91c9c4cef5fd92b2425c13d380
sebmichalowski <seb.michalowski@gmail.com>
2017-02-25 19:58:28 +0100

    docstring login_model classmethod changed to statick

--- model/login_model.py
+++ model/login_model.py
@@ -5,14 +5,14 @@ class LoginModel:
     """
     Validate user in database
     """
-    @classmethod
-    def check_user_in_db_by_mail_model(cls, mail, password):
+    @staticmethod
+    def check_user_in_db_by_mail_model(mail, password):
         """
         Check if user exists in database.
         :param mail:
         :param password:
-        :return: list
+        :return: list[ID, MAIL, ACCOUNT_TYPE]
         """
-        sql_obj = sql.query("SELECT ID, MAIL,ACCOUNT_TYPE FROM USERS WHERE MAIL=? AND PASSWORD=?;", [mail, password])
+        sql_obj = sql.query("SELECT ID, MAIL, ACCOUNT_TYPE FROM USERS WHERE MAIL=? AND PASSWORD=?;", [mail, password])
         mail_password_list = [sql_obj[0][0], sql_obj[0][1], sql_obj[0][2]]
         return mail_password_list
